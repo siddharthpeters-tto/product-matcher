db1ab0b16cc05c54e0cb0a0509c02f594512f155
Siddharth Peters <siddharth_peters@hotmail.com>
2025-08-20 17:04:03 +0400

    Changing to Supabase Anon Key

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ print(f"✅ CLIP model loaded, embedding dim: {clip_dim}")
 SUPABASE_URL = os.getenv("SUPABASE_URL", "")
 # Use anon key for public, read-only access guarded by RLS.
 SUPABASE_URL = "https://rffqzfdzosambdxmpuac.supabase.co/"
-SUPABASE_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmZnF6ZmR6b3NhbWJkeG1wdWFjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NTY2NTUxNiwiZXhwIjoyMDcxMjQxNTE2fQ.l9wlyn_1_GE4TTdQADbbdlZcn-EKZjb0hnwhnTvOGlY"
+SUPABASE_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmZnF6ZmR6b3NhbWJkeG1wdWFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2NjU1MTYsImV4cCI6MjA3MTI0MTUxNn0.bNj0M4SwVT0SVRVCFarBJLtBS-nYrSM7ZsZ_nGsMR5U"
 #SUPABASE_KEY = os.getenv("SUPABASE_ANON_KEY", "")
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise RuntimeError("Missing SUPABASE_URL or SUPABASE_ANON_KEY in env.")
