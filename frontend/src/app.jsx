5d2bec4eb4d8b919388c89b416cd9d74297aa739
siddharthpeters-tto <siddharth_peters@hotmail.com>
2026-03-18 13:10:10 +0400

    Testing FrontEnd App

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,7 +1,12 @@
+import UI from "./UI.jsx";
+
 export default function App() {
   return (
-    <div style={{ padding: 40, color: "white", background: "red", fontWeight: "bold" }}>
-      App is rendering
-    </div>
+    <>
+      <div style={{ padding: 40, color: "white", background: "red", fontWeight: "bold" }}>
+        App is rendering
+      </div>
+      <UI />
+    </>
   );
 }
