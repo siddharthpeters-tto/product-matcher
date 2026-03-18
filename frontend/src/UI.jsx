08ed98780c2022355e865dc4c6cf0d336f0900f6
siddharthpeters-tto <siddharth_peters@hotmail.com>
2026-03-18 13:19:28 +0400

    Faking UI.jsx - Test2

--- frontend/src/UI.jsx
+++ frontend/src/UI.jsx
@@ -1,4 +1,9 @@
+import React, { useState } from "react";
+import Filters, { DEFAULT_FILTERS } from "./Filters.jsx";
+
 export default function UI() {
+  const [filters] = useState(DEFAULT_FILTERS);
+
   return (
     <div style={{ padding: 24, background: "white", color: "black" }}>
       UI mounted
