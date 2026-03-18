c46978c205c9965fe01ee68f19572998256c67b1
siddharthpeters-tto <siddharth_peters@hotmail.com>
2026-03-18 14:12:08 +0400

    Faking UI.jsx - Test3

--- frontend/src/UI.jsx
+++ frontend/src/UI.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import Filters, { DEFAULT_FILTERS } from "./Filters.jsx";
+import { searchOneFile, searchTextOnly, runBatch } from "./ProductSearch.jsx";
 
 export default function UI() {
   const [filters] = useState(DEFAULT_FILTERS);
+
+  console.log(searchOneFile, searchTextOnly, runBatch);
 
   return (
     <div style={{ padding: 24, background: "white", color: "black" }}>
