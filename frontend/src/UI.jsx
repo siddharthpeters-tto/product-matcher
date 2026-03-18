557dcb160bd279f854474a011769d22142ab688a
siddharthpeters-tto <siddharth_peters@hotmail.com>
2026-03-18 14:15:07 +0400

    Faking UI.jsx - Test4

--- frontend/src/UI.jsx
+++ frontend/src/UI.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import Filters, { DEFAULT_FILTERS } from "./Filters.jsx";
 import { searchOneFile, searchTextOnly, runBatch } from "./ProductSearch.jsx";
+import { exportCartToPdf } from "./exportpdf.jsx";
 
 export default function UI() {
   const [filters] = useState(DEFAULT_FILTERS);
 
-  console.log(searchOneFile, searchTextOnly, runBatch);
+  console.log(searchOneFile, searchTextOnly, runBatch, exportCartToPdf);
 
   return (
     <div style={{ padding: 24, background: "white", color: "black" }}>
-      UI mounted
+      UI mounts
     </div>
   );
 }
